353eff126d4b32c4d482fdc35d80f6731e607890
James Stone <jamesmstone@users.noreply.github.com>
2021-02-06 16:02:40 +1100

    set hostname

--- example/configure.sh
+++ example/configure.sh
@@ -23,6 +23,9 @@ EOF
 ln -s networking /etc/init.d/net.lo
 ln -s networking /etc/init.d/net.eth0
 
+step 'set hostname'
+echo "01.jamesst.one" > /etc/hostname
+
 step 'Adjust rc.conf'
 sed -Ei \
 	-e 's/^[# ](rc_depend_strict)=.*/\1=NO/' \
